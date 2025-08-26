e6ec67a6006b7a02b6f400c4aa6d7d1042ae3529
Adam01-i <seckmote@gmail.com>
2025-08-26 22:30:22 +0000

    Feature-critiques

--- cinecritique-frontend-vite/src/App.tsx
+++ cinecritique-frontend-vite/src/App.tsx
@@ -64,7 +64,7 @@ const App: React.FC = () => {
             path="/my-reviews"
             element={
               <PrivateRoute>
-                <ReviewsPage mode="mine" />
+                <ReviewsPage />
               </PrivateRoute>
             }
           />
